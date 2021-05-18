4e842dfb7d4c3c8b04d3372a9f7ca144d630b8f0
praveengithub1985 <59757886+praveengithub1985@users.noreply.github.com>
2021-05-18 01:52:56 -0500

    Update AbstractClass1.java

--- GitFramework1/src/AbstractClass1.java
+++ GitFramework1/src/AbstractClass1.java
@@ -19,6 +19,10 @@ abstract class SuperClass
 	{
 		System.out.println("This is a Concrete Method in the super class");
 	}
+	void upload()
+	{
+		System.out.println("This is a Concrete Method in the super class");
+	}
 }
 
 public class AbstractClass1 extends SuperClass
